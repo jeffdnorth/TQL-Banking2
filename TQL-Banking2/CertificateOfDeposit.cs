3a5efb716ac4ed2673d4895b1fcfeeb47a8b18f3
Jeff North <jnorth@tql.com>
2021-05-25 08:48:38 -0400

    working on this

--- TQL-Banking2/CertificateOfDeposit.cs
+++ TQL-Banking2/CertificateOfDeposit.cs
@@ -12,16 +12,63 @@ namespace TQL_Banking2
         public int Months { get; set; }
 
         public override bool Deposit(decimal amount)
-        {
+             {
             Console.WriteLine("Cannot call Deposit() on CD");
-        }
             return false;
-        {
+             }
         public override bool Withdraw(decimal amount)
-        {
+             {
             Console.WriteLine("Call Withdraw() after WithdrawDate to recieve all funds");
+            return false;
+             }
+        public bool Withdraw()
+        {   
+            if(DateTime.Now < WithdrawDate)
+            {
+ Console.WriteLine($"Cannot withdraw funds from CD till {WithdrawDate.ToString("MM/dd/yyyy")}");
+                return false;
+            }
+            return base.Withdraw(Balance);
         }
+        private void CalcAndPayInterestOnDeposit()
+        {
+            var interest = Balance * (InterestRate / 12) * Months;
+            base.Deposit(decimal.Round(interest, 2));
+        }
+        private void  SetDurationAndRate(int months)
+        {
+            switch(months)
+            {
+                case 12:
+                    WithdrawDate = DepositDate.AddYears(1);
+                    InterestRate = 0.01m;
+                    break;
+                case 24:
+                    WithdrawDate = DepositDate.AddYears(2);
+                    InterestRate = 0.02m;
+                    break;
+                case 36:
+                    WithdrawDate = DepositDate.AddYears(3);
+                    InterestRate = 0.3m;
+                    break;
+                case 48:
+                    WithdrawDate = DepositDate.AddYears(4);
+                    InterestRate = 0.04m;
+                    break;
+                case 60:
+                    WithdrawDate = DepositDate.AddYears(5);
+                    InterestRate = 0.05m;
+                    break;
+                default:
+                    break;
+            }
+        }
+            public CertificateOfDeposit(decimal Amount, int Months)
+              {
+            this.Months = Months;
+            SetDurationAndRate(Months);
+            var success = base.Deposit(Amount);
+            CalcAndPayInterestOnDeposit();
+              }
     }       
-    {
-    }
 }
---
+++ TQL-Banking2/CertificateOfDeposit2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TQL_Banking2
+{
+    class CertificateOfDeposit2
+    {
+    }
+}
